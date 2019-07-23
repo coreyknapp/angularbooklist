0ec0b48a24583660780c29e736231a37db5c7436
Corey Knapp <cknapp.code@gmail.com>
2019-07-23 15:19:20 -0400

    Setup skeleton of components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ViewBooksComponent } from './view-books/view-books.component';
+import { AddBookComponent } from './add-book/add-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewBooksComponent,
+    AddBookComponent
   ],
   imports: [
     BrowserModule
